568e1786ff517abb7937ea61f30a99565246dc1b
ionthedev <brandon@friendlystudios.net>
2024-07-27 08:14:33 -0500

    Initial Source Code Commit.
    Player class is currently broken. Fixing

---
+++ src/YourGame.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Brandon Friend on 7/24/24.
+//
+
+#include "YourGame.h"
+
+YourGame::YourGame() = default;
+
+
+void YourGame::Start() const {}
+void YourGame::Update(double _deltaTime) const
+{
+}
+
+void YourGame::FixedUpdate(double _deltaTime) const
+{
+}
+
+void YourGame::Render() const {}
+void YourGame::HandleInput() const {}
+
