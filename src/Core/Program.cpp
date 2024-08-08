d53beae870b67bad0f21f2ff479ce23b8c51d146
ionthedev <brandon@friendlystudios.net>
2024-08-07 23:39:20 -0500

    Created Phantom Math class and Program Class

---
+++ src/Core/Program.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Brandon Friend on 8/7/24.
+//
+
+#include "Program.h"
---
+++ src/Core/Program.h
@@ -0,0 +1,16 @@
+//
+// Created by Brandon Friend on 8/7/24.
+//
+
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+
+
+class Program {
+
+};
+
+
+
+#endif //PROGRAM_H
