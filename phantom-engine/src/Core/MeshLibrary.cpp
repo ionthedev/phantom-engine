e272e4801f8267be4878b3de2801963793e090e3
Brandon <brandon@friendlystudios.net>
2024-08-15 23:27:20 -0500

    Created mesh library class. Needs to be actually designed.

---
+++ phantom-engine/src/Core/MeshLibrary.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Brandon Friend on 8/15/24.
+//
+
+#include "MeshLibrary.h"
+
+namespace Phantom {
+} // Phantom
---
+++ phantom-engine/src/Core/MeshLibrary.h
@@ -0,0 +1,19 @@
+//
+// Created by Brandon Friend on 8/15/24.
+//
+
+#ifndef MESHLIBRARY_H
+#define MESHLIBRARY_H
+
+namespace Phantom {
+
+class MeshLibrary {
+public:
+
+    std::vector<Model>Library;
+
+};
+
+} // Phantom
+
+#endif //MESHLIBRARY_H
